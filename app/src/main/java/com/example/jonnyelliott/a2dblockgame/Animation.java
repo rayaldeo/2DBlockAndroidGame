98d38596e6f382ee57728674c1c0b41f3190336e
Jonathan Elliott <jelliot@peoplenetonline.com>
2017-01-02 15:03:49 -0600

    Added Little Walking Alien Animation

--- app/src/main/java/com/example/jonnyelliott/a2dblockgame/Animation.java
+++ app/src/main/java/com/example/jonnyelliott/a2dblockgame/Animation.java
@@ -1,6 +1,9 @@
 package com.example.jonnyelliott.a2dblockgame;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 /**
  * Created by Jonny Elliott on 1/1/2017.
@@ -13,12 +16,19 @@ public class Animation {
 
     private boolean isPlaying= false;
 
-    public boolean isPlayed(){
+    public void play(){
+        isPlaying= true;
+        frameIndex=0;
+        lastFrame = System.currentTimeMillis();
+    }
+
+    public boolean isPlaying(){
         return isPlaying;
     }
 
     public void stop(){
         isPlaying= false;
+
     }
 
     private float frameTime;
@@ -33,12 +43,32 @@ public class Animation {
 
     }
 
+    public void draw(Canvas canvas, Rect destination){
+        if(!isPlaying){
+            return;
+        }
+        scaleRect(destination);
+        canvas.drawBitmap(frames[frameIndex],null, destination, new Paint());
+    }
     public void update(){
+
+        if(!isPlaying)
+            return;
         if(System.currentTimeMillis()-lastFrame>frameTime*1000){
             frameIndex++;
             //Ternary Statement in Java
             frameIndex = frameIndex>= frames.length ? 0:frameIndex;
+            lastFrame =System.currentTimeMillis();
 
+        }
+    }
+
+    public void scaleRect(Rect rect){
+        float whRatio = (float)frames[frameIndex].getWidth()/frames[frameIndex].getHeight();
+        if(rect.width()>rect.height()){
+            rect.left = rect.right-(int)(rect.height()*whRatio);
+        }else{
+            rect.top = rect.bottom-(int)(rect.width()*(1/whRatio));
         }
     }
 }
--- app/src/main/java/com/example/jonnyelliott/a2dblockgame/RectPlayer.java
+++ app/src/main/java/com/example/jonnyelliott/a2dblockgame/RectPlayer.java
@@ -1,7 +1,9 @@
 package com.example.jonnyelliott.a2dblockgame;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.Matrix;
 import android.graphics.Point;
 import android.graphics.Rect;
 
@@ -13,29 +15,56 @@ public class RectPlayer implements GameObject{
 
     private Rect rectangle;
     private int color;
+    private Animation idle,walkRight;
+    private Animation walkLeft;
+    private BitmapFactory bf  =new BitmapFactory();
+    private AnimationManager animManager;
 
     public RectPlayer(Rect rectangle,int color){
         this.rectangle =rectangle;
         this.color=color;
-    }
+        //Creating New Animation in Constructor
+        Bitmap idleImg = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(),R.drawable.idle);
+        Bitmap walksmallStep= bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(),R.drawable.walksmallstep);
+        Bitmap walkBigStep =bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(),R.drawable.walkwidestep);
+        idle = new Animation(new Bitmap[]{idleImg},2);
+        walkRight= new Animation (new Bitmap[]{walksmallStep,walkBigStep},0.5f);
+        Matrix m = new Matrix();
+        m.preScale(-1, 1);
+        Bitmap walksmallStep_left = Bitmap.createBitmap(walksmallStep, 0, 0, walksmallStep.getWidth(), walksmallStep.getHeight(), m, false);
+        Bitmap walkBigStep_left =Bitmap.createBitmap(walkBigStep, 0, 0, walkBigStep.getWidth(), walkBigStep.getHeight(), m, false);
+        walkLeft= new Animation (new Bitmap[]{walksmallStep_left,walkBigStep_left},0.5f);
 
+        animManager = new AnimationManager(new Animation[]{idle,walkRight,walkLeft});
+    }
     @Override
     public void draw(Canvas canvas) {
-        Paint paint = new Paint();
-        paint.setColor(this.color);
-        canvas.drawRect(this.rectangle,paint);
+//        Paint paint = new Paint();
+//        paint.setColor(this.color);
+//        canvas.drawRect(this.rectangle,paint);
+        animManager.draw(canvas,rectangle);
 
     }
 
     @Override
     public void update() {
-
+        animManager.update();
     }
 
     public void update(Point point) {
+        float oldLeft = rectangle.left;
         //left,top,right,bottom
         rectangle.set(point.x-rectangle.width()/2,point.y-rectangle.height()/2,
                 point.x+rectangle.width()/2,point.y+rectangle.height()/2);
+        int state = 0;
+        if(rectangle.left - oldLeft>5)
+            state =1;
+        else if(rectangle.left - oldLeft<-5)
+            state =2;
+
+
+        animManager.playAnim(state);
+        animManager.update();
     }
 
     public Rect getRectangle(){
